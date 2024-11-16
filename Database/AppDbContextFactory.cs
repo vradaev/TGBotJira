2349929f81cea9a3ae7c2841bb712e7d1b6dcc93
vradaev <vadimradaev@gmail.com>
2024-11-16 16:07:57 +0300

    database migration fix

--- Database/AppDbContextFactory.cs
+++ Database/AppDbContextFactory.cs
@@ -7,9 +7,12 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
+        var configFilePath = "config.json";
+        var config = new Config(configFilePath);
+        var connectionString = config.ConnectionStrings.DefaultConnection;
+
+
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        
-        var connectionString = "Host=172.17.0.1;Port=5433;Database=TelegramJiraDB;Username=postgres;Password=mysecretpassword";
         optionsBuilder.UseNpgsql(connectionString);
 
         return new AppDbContext(connectionString);
